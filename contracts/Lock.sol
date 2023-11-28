c9d114fd9d8026a6922961c8ea6033ebdf06895b
aditya172926 <aditya26sg@gmail.com>
2023-11-29 06:33:44 +0800

    updates

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.18;
 
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 // Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
